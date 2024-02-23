74f1654f14c8bd1dae0fd663cbca01a152a5f134
uzidreamcode <77190656+uzidreamcode@users.noreply.github.com>
2024-02-23 15:28:55 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,15 @@ Route::get('/hello', function () { return 'Hello World';
 Route::get('/world', function () { return 'World';
 });
 
+Route::get('/user/{name}', function ($name) {
+    return 'My name is '.$name;
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+
+
 
