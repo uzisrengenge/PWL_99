a3916c8b33e68805070453a3e8e35866b32c4fb3
uzidreamcode <77190656+uzidreamcode@users.noreply.github.com>
2024-02-24 20:39:50 +0700

    j

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,19 @@ Route::get('/hello', function () { return 'Hello World';
 
 Route::get('/world', function () { return 'World';
 });
+Route::get('/about', function () { return '2241270076 Muhammad Nurfauzi Ikhsan';
+});
 
 Route::get('/user/{name}', function ($name) {
     return 'My name is '.$name;
     });
 
-Route::get('/posts/{post}/comments/{comment}', function
+Route::get('/posts/{post?}/comments/{comment?}', function
     ($postId, $commentId) {
      return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
     });
 
-
-
+Route::get('/user/{name?}', function ($name=null) {
+        return 'My name is '.$name;
+        });
 
